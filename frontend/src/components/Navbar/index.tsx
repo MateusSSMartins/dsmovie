3fe2095b61986326f68f6b31ddebaa569aafb0be
Mateus <mateus_ss_martins@estudante.sc.senai.br>
2022-01-13 19:33:54 -0300

    Rotas

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,11 +7,12 @@ function Navbar() {
       <nav className='container'>
         <div className='dsmovie-nav-content'>
           <h1> DSMovie</h1>
-          <a href="https://github.com/"></a>
-          <div className='dsmovie-contact-container'>
-            <GitHubIcon />
-            <p className='dsmovie-contact-link'>/devsuperior</p>
-          </div>
+          <a href="https://github.com/devsuperior/MateusSSMartins" >
+            <div className='dsmovie-contact-container'>
+              <GitHubIcon />
+              <p className='dsmovie-contact-link'>/devsuperior</p>
+            </div>
+          </a>
         </div>
       </nav>
     </header>
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,8 @@
+function listing() {
+    return (
+        <h1>página de listagem</h1>
+    );
+
+}
+
+export default listing;
